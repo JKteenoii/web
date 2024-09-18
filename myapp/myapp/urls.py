3662de7f8d240ff4fb89b7125bbb678afeeeec8a
JKteenoii <jakkree.teenoi@gmail.com>
2024-09-18 14:51:53 +0700

    update

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .viwes import index
+from .viwes import *
 urlpatterns = [
     path('',index, name='index'),   
     path('admin/', admin.site.urls),
+    path('form/',form), 
     
 ]
